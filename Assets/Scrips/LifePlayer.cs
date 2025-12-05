1340c648da2cba192d85f8c12eb3699d71e47d87
Kime_MangaX <kimemanga@gmail.com>
2025-12-04 20:13:13 -0500

    Create LifePlayer.cs

---
+++ Assets/Scrips/LifePlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LifePlayer : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
