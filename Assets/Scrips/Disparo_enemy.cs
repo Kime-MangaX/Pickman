330d5bd5e920fc13e72af6bbf58b0f84c4ce68b5
Kime_MangaX <kimemanga@gmail.com>
2025-12-10 21:52:25 -0500

    Update Disparo_enemy.cs

--- Assets/Scrips/Disparo_enemy.cs
+++ Assets/Scrips/Disparo_enemy.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 
 public class Disparo_enemy : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletFants;
-    [SerializeField] private float tiempo;
-
+ 
     void Start()
     {
-        
+       
     }
 
 }
