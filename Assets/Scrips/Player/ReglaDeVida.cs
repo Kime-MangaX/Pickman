911d2933ad69e8f4400860e7ddb1aa5b4498016e
Kime_MangaX <kimemanga@gmail.com>
2025-12-11 18:44:51 -0500

    Update ReglaDeVida.cs

--- Assets/Scrips/Player/ReglaDeVida.cs
+++ Assets/Scrips/Player/ReglaDeVida.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class ReglaDeVida : MonoBehaviour
 {
     [SerializeField] public int life = 10;
-    [SerializeField] public GameObject Enemy;
 
     private int Collider2D;
    
@@ -17,22 +16,12 @@ public class ReglaDeVida : MonoBehaviour
       
     }
 
-    /*public void EnemyDamage()
+    public void EnemyDamage()
     {
         print("Resive Daño");
-        life--;
-    }*/
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        print("Enemy Colision : " + collision.tag);
-        if (collision.tag == "Destrulle")
-        {
-            life--;
-            if (life <= 0)
-                Destroy(gameObject);
-            print("Perdiste");
-        }
+        life-= 2;
     }
+
+    
 
 }
