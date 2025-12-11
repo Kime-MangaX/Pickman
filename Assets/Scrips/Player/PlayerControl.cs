6519a8a73d2fbfdc5d2592a840ae6e8f13ba6722
Kime_MangaX <kimemanga@gmail.com>
2025-12-10 23:11:56 -0500

    Update Hoja_Mapa.unity

--- Assets/Scrips/Player/PlayerControl.cs
+++ Assets/Scrips/Player/PlayerControl.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public GameObject BulletPrefab;
-    [SerializeField] public int life = 10;
+  
 
     public float Speed;
 
---
+++ Assets/Scrips/Player/ReglaDeVida.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ReglaDeVida : MonoBehaviour
+{
+    [SerializeField] public int life = 10;
+    [SerializeField] public int damage;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void EnemyDamage()
+    {
+
+    }
+}
